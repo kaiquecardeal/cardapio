6159d36107d8d9e36b9d8b0a6655cbe28ea3391d
InnOver <kaiquedaniel@hotmail.com>
2024-01-07 02:34:23 -0300

    Quarto commit - Metodo Post

--- src/main/java/com/example/cardapio/controller/FoodController.java
+++ src/main/java/com/example/cardapio/controller/FoodController.java
@@ -2,12 +2,10 @@ package com.example.cardapio.controller;
 
 import com.example.cardapio.food.Food;
 import com.example.cardapio.food.FoodRepository;
+import com.example.cardapio.food.FoodRequestDTO;
 import com.example.cardapio.food.FoodResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,9 +16,12 @@ public class FoodController {
     @Autowired
     private FoodRepository repository;
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PostMapping
-    public void saveFood(){
-
+    public void saveFood(@RequestBody FoodRequestDTO data){
+        Food foodData = new Food(data);
+        repository.save(foodData);
+        return;
     }
     @GetMapping
     public List<FoodResponseDTO> getAll(){
